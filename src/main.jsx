06045bb91303543e521ac6a56340b27c65ccc6b5
Cecile-S <162564092+Cecile-S@users.noreply.github.com>
2026-07-27 01:12:14 +0200

    fix: wrap app with EquipmentProvider and DocumentsProvider too, alongside PropertyProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { PropertyProvider } from './hooks/useProperty'
+import { EquipmentProvider } from './hooks/useEquipment'
+import { DocumentsProvider } from './hooks/useDocuments'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <PropertyProvider>
-        <App />
+        <EquipmentProvider>
+          <DocumentsProvider>
+            <App />
+          </DocumentsProvider>
+        </EquipmentProvider>
       </PropertyProvider>
     </BrowserRouter>
   </React.StrictMode>
